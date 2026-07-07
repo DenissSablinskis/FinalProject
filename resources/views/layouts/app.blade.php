963628fa00606a061f07c42efc77569d3af750d8
Deniss Sablinskis <denissablinskij@gmail.com>
2026-07-07 19:15:52 +0300

    Refactor sessions migration and update home page layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,7 @@
 
         <!-- Galvenā satura sadaļa -->
         <main>
-            <h1 data-translate="heading">Par uzņēmumu</h1>
-            <p data-translate="text">GAISMA pārtikas piegādes pasūtīšanas sistēma ir moderns risinājums ērtai un ātrai pārtikas produktu iegādei. Sistēma ļauj klientiem pārlūkot plašu pārtikas 
-               preču katalogu ar precīziem aprakstiem un cenām, viegli veidot un pārvaldīt iepirkumu grozu, izvēlēties piegādes laiku un adresi, kā arī saņemt pasūtījuma 
-               apstiprinājumu un piegādes statusa atjauninājumus.</p>
-            <img src="{{ asset('images/main-img-mobile.jpg') }}" alt="Uzņēmuma logo">
+            @yield('content')
         </main>
 
         <!-- Kājene -->
