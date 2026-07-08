a111e06a0177a69a1064e1dbb9e509a76ff4ed25
Deniss Sablinskis <denissablinskij@gmail.com>
2026-07-08 19:19:29 +0300

    Add SetLocale middleware to manage user locale settings

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetLocale
+{
+    /**
+     * Parbauda lietotāja lokali un piešķir to esošam HTTP vaicājumam.
+     * Ja sessijā lokales vēl nav, tiek piešķirta defolta vertība.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $allowedLocales = ['lv', 'en'];
+        $locale = session('locale', config('app.locale'));
+
+        // Aizsardība no neeksistējošam lokalēm, piemēram, ievadītām adresu joslā
+        if (!in_array($locale, $allowedLocales, true)) {
+            $locale = config('app.locale');
+        }
+
+        App::setLocale($locale);  
+
+        return $next($request);
+    }
+}
