c7ab3b0821504f304e217e136005f59afb4743c7
Deniss Sablinskis <denissablinskij@gmail.com>
2026-07-08 09:51:10 +0300

    Update theme icon paths and fix HTML doctype in layout files

--- resources/views/partials/nav/guest.blade.php
+++ resources/views/partials/nav/guest.blade.php
@@ -1,3 +1,4 @@
 <a href="#" class="active-link" data-translate="navHome">Galvenā lapa</a>
 <a href="#" data-translate="navProducts">Produkti</a>
 <a href="#" data-translate="navLogin">Ieiet</a>
+<a href="#" data-translate="navRegister">Reģistrēties</a>
