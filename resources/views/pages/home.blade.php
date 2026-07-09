ccd36fe212b52a58beb4397c203897d13fec5559
Deniss Sablinskis <denissablinskij@gmail.com>
2026-07-09 17:31:13 +0300

    Enhance layout and responsiveness by adding media queries and restructuring image display in home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,5 +3,8 @@
 @section('content')
     <h1>{{ __('messages.heading') }}</h1>
     <p data-translate="text">{{ __('messages.text') }}</p>
-    <img src="{{ asset('images/main-img-mobile.jpg') }}" alt="Uzņēmuma logo">
+    <picture class="about-image-wrapper">
+        <source media="(min-width: 580px)" srcset="{{ asset('images/main-img.jpg') }}">
+        <img src="{{ asset('images/main-img-mobile.jpg') }}" alt="Uzņēmuma logo">
+    </picture>
 @endsection
