4bb54d4cce8b58d158cb61484fa89827daab52d1
Deniss Sablinskis <denissablinskij@gmail.com>
2026-07-09 11:18:48 +0300

    Add locale switching route

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/locale/switch/{locale}', function (Request $request, string $locale) {
+    $allowedLocales = ['lv', 'en'];
+
+    if (in_array($locale, $allowedLocales, true)) {
+        $request->session()->put('locale', $locale);
+    }
+
+    return redirect()->back();
+})->name('locale.switch');
 
 Route::get('/home', function () {
     return view('pages.home');
