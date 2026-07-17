6db3797fc0f894c3486cac08aa5bfefba12bd871
Deniss Sablinskis <denissablinskij@gmail.com>
2026-07-17 15:01:47 +0300

    Add initial login form

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,1 +1,33 @@
 @extends('layouts.app')
+
+@section('content')
+    <form method='post' action="{{ route('login.store') }}">
+        @csrf
+        @if ($errors->any())
+            <div>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <label for="email">Ievadiet e-pasta adresi</label>
+        <input type="text" id="email" name="email" value="{{ old('email') }}" required>
+
+        @error('email')
+            <div>{{ $message }}</div>
+        @enderror
+
+        <label for="password">Ievadiet paroli</label>
+        <input type="password" id="password" name="password" required>
+
+        @error('password')
+            <div>{{ $message }}</div>
+        @enderror
+        
+        <input type="submit" value="Ieiet">
+    </form>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ Route::post('/locale/switch/{locale}', function (Request $request, string $local
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login.store');
     Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/register', [AuthController::class, 'register'])->name('register.store');
 });
 
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
